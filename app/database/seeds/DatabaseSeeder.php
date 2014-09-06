59be2b2310fcd5b96c49d163029c4ae1912cfab4
Kamal dev Bhuyan <kamaldev.bhuyan@gmail.com>
2014-09-06 22:00:19 +0530

    database schema migrations

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -10,8 +10,32 @@ class DatabaseSeeder extends Seeder {
 	public function run()
 	{
 		Eloquent::unguard();
+		//$this->call('UserTableSeeder');
+		//$this->command->info('User table seeded!');
 
-		// $this->call('UserTableSeeder');
+		$this->call('ContactsTableSeeder');
+		$this->command->info('Contacts table seeded!');
 	}
+}
+/*
+class UserTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('users')->delete();
+        
+        User::create(array('email' => 'foo@bar.com', 'password'=> Hash::make('demo')));
+    }
+
+}
+*/
+class ContactsTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('contacts')->delete();
+        
+        Contact::create(array('name' => 'Kamal Dev', 'email' => 'foo@bar.com', 'mobile'=> '8011877042'));
+    }
 
 }
